1d50f5700aeb6f507c16335040c8860c4bbd1dc0
amrrs <amrrs@outlook.com>
2018-09-27 15:00:02 +0530

    some theme change

--- _build.sh
+++ _build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+Rscript -e "bookdown::render_book('index.Rmd', bookdown::html_book())"
 
